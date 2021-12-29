20d43a913f7789c349cd04ca1b4d47787c0722c3
Stoyann <stoyann.openclassrooms@gmail.com>
2021-12-29 15:00:20 +1100

    ajout algo

--- APP/class/RecipesList.js
+++ APP/class/RecipesList.js
@@ -44,9 +44,7 @@ export class RecipesList {
     return [...AllUstensils];
   }
 
-  //fonction de recherche dans la liste recipes (retourne la listes des recettes trier)
-
-  // ================================ algo de recherche numero 1
+  // ================================ algo de recherche numero 2 boucle for
 
   // search(request) {
   //   let sortedRecipes = [];
@@ -85,32 +83,4 @@ export class RecipesList {
 
   //   return this.recipes;
   // }
-
-  // ================================ algo de recherche numero 2
-
-  search(request) {
-    let sortedRecipes = [];
-
-    if (request.tags.length > 0) {
-      for (let tag in request.tags) {
-        sortedRecipes = this.recipes.filter(
-          (recipe) =>
-            recipe.stringifyRecipes.includes(
-              Utils.removeAccents(request.tags[tag])
-            ) &&
-            recipe.stringifyRecipes.includes(
-              Utils.removeAccents(request.userInput)
-            )
-        );
-      }
-    } else if (request.userInput != "") {
-      sortedRecipes = this.recipes.filter((recipe) =>
-        recipe.stringifyRecipes.includes(Utils.removeAccents(request.userInput))
-      );
-    }
-
-    this.recipes = [...new Set(sortedRecipes)];
-
-    return this.recipes;
-  }
 }
